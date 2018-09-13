f3d0f07a64652c1bb808f5d3555514a750a15898
Mafi <maros.filip@gmail.com>
2018-09-13 19:14:25 +0200

    Branches merged to master, unused imports removed

--- app/src/main/java/sk/groupin/groupin/MainActivity.java
+++ app/src/main/java/sk/groupin/groupin/MainActivity.java
@@ -8,36 +8,22 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.graphics.Bitmap;
 import android.graphics.Color;
-import android.net.wifi.hotspot2.pps.Credential;
 import android.os.Build;
-import android.support.v4.content.LocalBroadcastManager;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.telephony.TelephonyManager;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
-import android.webkit.CookieManager;
 import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
-import android.webkit.WebViewDatabase;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.ProgressBar;
-import android.widget.TextView;
 import android.widget.Toast;
 
-import java.util.List;
-
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
-import retrofit2.Retrofit;
-import retrofit2.http.Body;
 
 public class MainActivity extends AppCompatActivity {
 
